115a4812449a4e2dfdb26e036f3b24cb59d2b3d7
urialon <urialon1@gmail.com>
2019-01-10 19:03:19 +0200

    Batch files extraction

--- JavaExtractor/JPredict/src/main/java/JavaExtractor/App.java
+++ JavaExtractor/JPredict/src/main/java/JavaExtractor/App.java
@@ -2,15 +2,23 @@ package JavaExtractor;
 
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
+import java.util.stream.Collectors;
 
 import org.kohsuke.args4j.CmdLineException;
 
 import JavaExtractor.Common.CommandLineValues;
 import JavaExtractor.FeaturesEntities.ProgramRelation;
+
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.LongStream.range;
 
 public class App {
 	private static CommandLineValues s_CommandLineValues;
@@ -33,24 +41,39 @@ public class App {
 	}
 
 	private static void extractDir() {
-		ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(s_CommandLineValues.NumThreads);
-		LinkedList<ExtractFeaturesTask> tasks = new LinkedList<>();
+		List<Path> files = null;
 		try {
-			Files.walk(Paths.get(s_CommandLineValues.Dir)).filter(Files::isRegularFile)
-					.filter(p -> p.toString().toLowerCase().endsWith(".java")).forEach(f -> {
-						ExtractFeaturesTask task = new ExtractFeaturesTask(s_CommandLineValues, f);
-						tasks.add(task);
-					});
+			files = Files.walk(Paths.get(s_CommandLineValues.Dir)).filter(Files::isRegularFile)
+					.filter(p -> p.toString().toLowerCase().endsWith(".java")).collect(toList());
 		} catch (IOException e) {
 			e.printStackTrace();
 			return;
 		}
-		try {
-			executor.invokeAll(tasks);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		} finally {
-			executor.shutdown();
+
+		int[] count = new int[1];
+		final int CHUNK_SIZE = 100;
+		Map<Integer, List<Path>> batchedFiles = files.stream().collect( Collectors.groupingBy(
+				file -> {
+					count[0]++;
+					return Math.floorDiv(count[0], CHUNK_SIZE);
+				} )
+		);
+
+		for (List<Path> filesBatch : batchedFiles.values()) {
+			ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(s_CommandLineValues.NumThreads);
+			LinkedList<ExtractFeaturesTask> tasks = new LinkedList<>();
+			filesBatch.forEach(f -> {
+				ExtractFeaturesTask task = new ExtractFeaturesTask(s_CommandLineValues, f);
+				tasks.add(task);
+			});
+
+			try {
+				executor.invokeAll(tasks);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} finally {
+				executor.shutdown();
+			}
 		}
 	}
 }
