a7063f90dfbbf1dcf972f9bca0b0afdff5ed0a74
Ubuntu <ubuntu@ip-172-31-51-188.ec2.internal>
2019-01-11 20:39:25 +0000

    train.sh

--- train.sh
+++ train.sh
@@ -5,13 +5,13 @@
 # test_data: by default, points to the validation set, since this is the set that
 #   will be evaluated after each training iteration. If you wish to test
 #   on the final (held-out) test set, change 'val' to 'test'.
-type=java14m
-dataset_name=java14m
+type=java-large
+dataset_name=java-large
 data_dir=data/${dataset_name}
 data=${data_dir}/${dataset_name}
-test_data=${data_dir}/${dataset_name}.val.c2v
+test_data=${data_dir}/${dataset_name}.test.c2v
 model_dir=models/${type}
 
-mkdir -p models/${model_dir}
+mkdir -p ${model_dir}
 set -e
 python3 -u code2seq.py --data ${data} --test ${test_data} --save ${model_dir}/saved_model
